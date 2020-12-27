8d358c0555a8e661655ce588cbd14a584f6873c6
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-12-27 11:44:22 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpastore/controller/OrderController.java
@@ -0,0 +1,59 @@
+package jpabook.jpastore.controller;
+
+import jpabook.jpastore.domain.Member;
+import jpabook.jpastore.domain.Order;
+import jpabook.jpastore.domain.item.Item;
+import jpabook.jpastore.repository.OrderSearch;
+import jpabook.jpastore.service.ItemService;
+import jpabook.jpastore.service.MemberService;
+import jpabook.jpastore.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    private final MemberService memberService;
+
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+        return "order/orderList";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+        orderService.cancelOrder(orderId);
+        return "redirect:/orders";
+    }
+}
